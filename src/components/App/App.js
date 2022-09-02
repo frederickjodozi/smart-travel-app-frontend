d429e5409133070b46d3c2cc68c1f6fe41fe4583
Fred Jodozi <fredfred776@gmail.com>
2022-09-02 19:26:03 -0400

    add backgroundImage to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
