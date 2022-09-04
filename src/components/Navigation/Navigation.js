e28c7772c5f0229859a40f02f36d84c389ed40ee
Fred Jodozi <fredfred776@gmail.com>
2022-09-04 10:43:57 -0400

    add navigation icons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,7 @@
 import { React } from 'react';
 import './Navigation.css';
+import homeIcon from '../../images/homeIcon.svg';
+import articlesIcon from '../../images/articlesIcon.svg';
 
 function Navigation({
   isLoggedIn, onLogout, onSignUpClick, onLoginClick
@@ -8,22 +10,24 @@ function Navigation({
     <nav className="navigation">
       {isLoggedIn ? (
         <ul className="navigation__list">
-          <li>
+          <li className="navigation__item">
+            <img src={homeIcon} className="navigation__icon" aria-label="home" />
             <a className="navigation__link" href="#home">Home</a>
           </li>
-          <li>
+          <li className="navigation__item">
+            <img src={articlesIcon} className="navigation__icon" aria-label="saved articles" />
             <a className="navigation__link" href="#savedarticles">Saved Articles</a>
           </li>
-          <li>
+          <li className="navigation__item">
             <button className="navigation__button" type="button" onClick="onLogout">Log Out</button>
           </li>
         </ul>
       ) : (
         <ul className="navigation__list">
-          <li>
+          <li className="navigation__item">
             <button className="navigation__button" type="button" onClick="onLoginClick">Log In</button>
           </li>
-          <li>
+          <li className="navigation__item">
             <button className="navigation__button" type="button" onClick="onSignUpClick">Sign Up</button>
           </li>
         </ul>
