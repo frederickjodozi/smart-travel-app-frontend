d1cbe7e2b992e0794893dfb45fa8d5fbc348c4f7
Fred Jodozi <fredfred776@gmail.com>
2022-09-04 10:52:57 -0400

    add markup and functionality for searchbar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import { React, useState } from 'react';
+import './SearchBar.css';
+
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = (e) => {
+    e.preventDefault();
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch({ query });
+  };
+
+  return (
+    <section className="searchbar">
+      <form className="searchbar__form" role="search" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="search"
+          className="searchbar__input"
+          aria-label="query"
+          value={query}
+          onChange={handleQueryChange}
+          placeholder="Your Search to anywhere and everywhere..."
+          minLength="2"
+          maxLength="30"
+          required
+        />
+        <button type="submit" className="searchbar__button">Go!</button>
+      </form>
+    </section>
+  );
+}
+
+export default SearchBar;
