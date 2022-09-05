0fad2e5f1ff0aa54881ea62aff953c1be692eba9
Fred Jodozi <fredfred776@gmail.com>
2022-09-04 21:50:28 -0400

    add states and handler functions for registration and queries

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import SignUpFormPopup from '../SignUpFormPopup/SignUpFormPopup';
+import LoginFormPopup from '../LoginFormPopup/LoginFormPopup';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [isSignUpFormOpen, setIsSignUpFormOpen] = useState(false);
+  const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleSignUpClick = () => {
+    setIsSignUpFormOpen(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsLoginFormOpen(true);
+  };
+
+  const handleClosePopups = () => {
+    setIsSignUpFormOpen(false);
+    setIsLoginFormOpen(false);
+  };
+
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    console.log('Submitted!');
+    handleClosePopups();
+  };
+
+  const handleQuery = () => {
+    console.log('Searching!');
+  };
+
   return (
     <div className="app">
-      <Header />
-      <Main />
+      <Header
+        isLoggedIn={isLoggedIn}
+        onLogout={handleLogout}
+        onSignUpClick={handleSignUpClick}
+        onLoginClick={handleLoginClick}
+      />
+      <Main onSearch={handleQuery} />
+      <SignUpFormPopup
+        isOpen={isSignUpFormOpen}
+        onClose={handleClosePopups}
+        onSubmit={handleRegistration}
+      />
+      <LoginFormPopup
+        isOpen={isLoginFormOpen}
+        onClose={handleClosePopups}
+        onSubmit={handleRegistration}
+      />
     </div>
   );
 }
