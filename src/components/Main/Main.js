e685560bbd261cd0b474716fe78f164eae5aa4aa
Fred Jodozi <fredfred776@gmail.com>
2022-09-13 18:21:30 -0400

    add react-router and route for search results in main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,40 @@
 import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import About from '../About/About';
+import Locations from '../Locations/Locations';
 import './Main.css';
 
 function Main({ onQuery }) {
   return (
-    <section className="main">
-      <h1 className="main__title">Travel Smart</h1>
-      <h2 className="main__subtitle">
-        Find amazing sightseeing locations from all over the world!
-      </h2>
-      <SearchBar onQuery={onQuery} />
-      <About />
-    </section>
+    <main className="main">
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <>
+              <h1 className="main__title">Travel Smart</h1>
+              <h2 className="main__subtitle">
+                Find amazing sightseeing locations from all over the world!
+              </h2>
+              <SearchBar onQuery={onQuery} />
+              <About />
+            </>
+        )}
+        />
+        <Route
+          path="/locations"
+          element={(
+            <>
+              <h2 className="main__subtitle">
+                Location results for your search:
+              </h2>
+              <Locations />
+            </>
+          )}
+        />
+      </Routes>
+    </main>
   );
 }
 
