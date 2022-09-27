18f26745989559f6058d53cb797247774484a6f5
Fred Jodozi <fredfred776@gmail.com>
2022-09-27 09:31:02 -0400

    refactor PopupForm to include Popup component

--- src/components/PopupForm/PopupForm.js
+++ src/components/PopupForm/PopupForm.js
@@ -1,40 +1,21 @@
-import { React, useEffect } from 'react';
+import { React } from 'react';
+import Popup from '../Popup/Popup';
 import './PopupForm.css';
 
 function PopupForm({
   isOpen, onClose, onSubmit, title, submitText, children
 }) {
-  useEffect(() => {
-    if (!isOpen) return;
-
-    const closeByEscape = (e) => {
-      if (e.key === 'Escape') {
-        onClose();
-      }
-    };
-
-    document.addEventListener('keydown', closeByEscape);
-    // eslint-disable-next-line consistent-return
-    return () => document.removeEventListener('keydown', closeByEscape);
-  }, [isOpen, onClose]);
-
-  const handleOverlay = (e) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
-  };
-
   return (
-    <div className={`popup ${isOpen ? 'popup_active' : ''}`} onClick={handleOverlay}>
+    <Popup isOpen={isOpen} onClose={onClose}>
       <div className="popup__box">
         <h3 className="popup__title">{title}</h3>
         <form className="popup__form" onSubmit={onSubmit}>
           {children}
           <button type="submit" className="popup__submit-button">{submitText}</button>
         </form>
-        <button className="popup__close-button" type="button" onClick={onClose}/>
+        <button className="popup__close-button" type="button" onClick={onClose} />
       </div>
-    </div>
+    </Popup>
   );
 }
 
