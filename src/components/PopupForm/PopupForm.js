8669b65c94ef36327d517ca0bfbc6b5b806d7b67
Fred Jodozi <fredfred776@gmail.com>
2022-09-27 10:19:34 -0400

    add aria-labels for buttons

--- src/components/PopupForm/PopupForm.js
+++ src/components/PopupForm/PopupForm.js
@@ -11,9 +11,20 @@ function PopupForm({
         <h3 className="popup__title">{title}</h3>
         <form className="popup__form" onSubmit={onSubmit}>
           {children}
-          <button type="submit" className="popup__submit-button">{submitText}</button>
+          <button
+            type="submit"
+            className="popup__submit-button"
+            aria-label="submit"
+          >
+            {submitText}
+          </button>
         </form>
-        <button className="popup__close-button" type="button" onClick={onClose} />
+        <button
+          className="popup__close-button"
+          type="button"
+          onClick={onClose}
+          aria-label="close popup"
+        />
       </div>
     </Popup>
   );
