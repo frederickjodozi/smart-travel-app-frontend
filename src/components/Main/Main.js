5f814409f7fb963f3ad33f1335a64e5df90c01ef
Fred Jodozi <fredfred776@gmail.com>
2022-09-27 10:28:36 -0400

    add about component, /saved-locations route and use ProtectedRoute

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,14 @@
 import { React } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import SearchBar from '../SearchBar/SearchBar';
 import About from '../About/About';
 import Locations from '../Locations/Locations';
 import './Main.css';
 
-function Main({ onQuery, locations }) {
+function Main({
+  onQuery, isLoggedIn, locations, savedLocations
+}) {
   return (
     <main className="main">
       <Routes>
@@ -18,19 +21,30 @@ function Main({ onQuery, locations }) {
                 Find amazing sightseeing locations from all over the world!
               </h2>
               <SearchBar onQuery={onQuery} />
-              {/* <About /> */}
+              <About />
             </>
         )}
         />
         <Route
           path="/locations"
           element={(
-            <>
+            <ProtectedRoute routeAuth={locations}>
               <h2 className="main__subtitle">
                 Location results for your search:
               </h2>
               <Locations locations={locations} />
-            </>
+            </ProtectedRoute>
+          )}
+        />
+        <Route
+          path="/saved-locations"
+          element={(
+            <ProtectedRoute routeAuth={isLoggedIn}>
+              <h2 className="main__subtitle">
+                Your saved locations:
+              </h2>
+              <Locations locations={savedLocations} />
+            </ProtectedRoute>
           )}
         />
       </Routes>
