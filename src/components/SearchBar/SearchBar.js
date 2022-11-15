5baa2c9bb10f3e3500658234bc9399b21346be9f
Fred Jodozi <fredfred776@gmail.com>
2022-11-15 16:17:32 -0500

    add validation functionality and error message display

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,32 +4,54 @@ import downArrow from '../../images/downArrow.png';
 
 function SearchBar({ onQuery }) {
   const [query, setQuery] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleQueryChange = (e) => {
-    e.preventDefault();
     setQuery(e.target.value);
   };
 
+  function handleValidation() {
+    if (!query) {
+      setErrorMessage('Please fill out the query');
+      return false;
+    }
+
+    if (query.length < 3) {
+      setErrorMessage('The query must contain at least 3 characters');
+      return false;
+    }
+
+    if (query.length > 30) {
+      setErrorMessage('The query must contain no more than 30 characters');
+      return false;
+    }
+
+    return true;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    onQuery(query);
+    if (handleValidation() === true) {
+      onQuery(query);
+      setErrorMessage('');
+    }
   };
 
   return (
     <section className="searchbar">
       <form className="searchbar__form" role="search" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="search"
-          className="searchbar__input"
-          aria-label="query"
-          value={query}
-          onChange={handleQueryChange}
-          placeholder="Your Search to anywhere..."
-          minLength="2"
-          maxLength="30"
-          required
-        />
+        <div className="searchbar__input-container">
+          <input
+            type="text"
+            id="search"
+            className="searchbar__input"
+            aria-label="query"
+            value={query}
+            onChange={handleQueryChange}
+            placeholder="Your Search to anywhere..."
+          />
+          {errorMessage && <span className="searchbar__error">{errorMessage}</span>}
+        </div>
         <button type="submit" className="searchbar__button" aria-label="submit">
           Go!
         </button>
