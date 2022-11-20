09cc8cdfa39ba777dea13d1ae2927fe927dd94b6
Fred Jodozi <fredfred776@gmail.com>
2022-11-20 16:48:39 -0500

    refactor states for inputs and add validation and error messages

--- src/components/SignUpFormPopup/SignUpFormPopup.js
+++ src/components/SignUpFormPopup/SignUpFormPopup.js
@@ -3,47 +3,81 @@ import PopupForm from '../PopupForm/PopupForm';
 import './SignUpFormPopup.css';
 
 function SignUpFormPopup({ isOpen, onClose, onSubmit }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    password: ''
+  });
 
-  const handleEmailChange = (e) => {
-    e.preventDefault();
-    setEmail(e.target.value);
+  const [errorMessages, setErrorMessages] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value
+    });
   };
 
-  const handlePasswordChange = (e) => {
+  async function handleValidation() {
+    const error = {};
+
+    if (!inputValues.email) {
+      error.email = 'Please enter an email address';
+    } else if (inputValues.email.length < 3) {
+      error.email = 'Please enter a valid email address';
+    }
+
+    if (!inputValues.password) {
+      error.password = 'Please enter a password';
+    } else if (inputValues.password.length < 6) {
+      error.password = 'Your password must contain at least 6 characters';
+    } else if (inputValues.password.length > 30) {
+      error.password = 'Your password must contain no more than 30 characters';
+    }
+
+    return error;
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setPassword(e.target.value);
+    handleValidation().then((error) => {
+      if (Object.keys(error).length === 0) {
+        onSubmit(inputValues);
+        setInputValues({
+          email: '',
+          password: ''
+        });
+        setErrorMessages({});
+      } else {
+        setErrorMessages(error);
+      }
+    });
   };
 
   return (
-    <PopupForm isOpen={isOpen} onClose={onClose} onSubmit={onSubmit} title="Sign Up" submitText="Create Profile">
+    <PopupForm isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit} title="Sign Up" submitText="Create Profile">
       <input
         type="text"
         name="email"
         id="signupform__email"
-        className="signupform__input"
+        className={`signupform__input ${errorMessages.email ? 'signupform__input-error' : ''}`}
         aria-label="email input"
-        value={email}
-        onChange={handleEmailChange}
+        value={inputValues.email}
+        onChange={handleInputChange}
         placeholder="Enter your email"
-        minLength="2"
-        maxLength="30"
-        required
       />
+      {errorMessages.email && <span className="signupform__error">{errorMessages.email}</span>}
       <input
         type="text"
         name="password"
         id="signupform__password"
-        className="signupform__input"
+        className={`signupform__input ${errorMessages.password ? 'signupform__input-error' : ''}`}
         aria-label="password input"
-        value={password}
-        onChange={handlePasswordChange}
+        value={inputValues.password}
+        onChange={handleInputChange}
         placeholder="Create a password"
-        minLength="2"
-        maxLength="30"
-        required
       />
+      {errorMessages.password && <span className="signupform__error">{errorMessages.password}</span>}
     </PopupForm>
   );
 }
