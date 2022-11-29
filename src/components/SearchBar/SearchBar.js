c7e6109480bc2391a0b4232861f9c18a819b7d59
Fred Jodozi <fredfred776@gmail.com>
2022-11-28 22:38:25 -0500

    add query type functionality for the user to define the category of locations to be returned

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './SearchBar.css';
+import smallTriangle from '../../images/smallTriangle.svg';
+import smallTriangleSelected from '../../images/smallTriangleSelected.svg';
+import singleDownArrow from '../../images/singleDownArrow.svg';
 import downArrow from '../../images/downArrow.png';
 
 function SearchBar({ onQuery }) {
+  // QUERY STATES //
   const [query, setQuery] = useState('');
+  const [queryType, setQueryType] = useState('interesting_places');
   const [errorMessage, setErrorMessage] = useState('');
 
+  // QUERY TYPE LIST DROPDOWN STATE AND EFFECT //
+  const [isQueryListOpen, setIsQueryListOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isQueryListOpen) return;
+
+    const closeByKey = (e) => {
+      if (e.key === ('Escape' || ' ')) {
+        setIsQueryListOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', closeByKey);
+
+    return () => {
+      document.removeEventListener('keydown', closeByKey);
+    };
+  }, [isQueryListOpen]);
+
+  // QUERY CHANGE HANDLERS //
   const handleQueryChange = (e) => {
     setQuery(e.target.value);
   };
 
+  const handleQueryTypeChange = (e) => {
+    setQueryType(e.target.value);
+    setIsQueryListOpen(false);
+  };
+
+  // QUERY TYPE BUTTON CLICK HANDLER //
+  const handleQueryListClick = () => {
+    if (isQueryListOpen) {
+      setIsQueryListOpen(false);
+      return;
+    }
+    setIsQueryListOpen(true);
+  };
+
+  // VALIDATION HANDLER //
   async function handleValidation() {
     let error = '';
 
@@ -24,12 +64,14 @@ function SearchBar({ onQuery }) {
     return error;
   }
 
+  // SUBMIT HANDLER //
   const handleSubmit = (e) => {
     e.preventDefault();
     handleValidation().then((error) => {
       if (error.length === 0) {
-        onQuery(query);
+        onQuery(query, queryType);
         setQuery('');
+        setQueryType('interesting_places');
       } else {
         setErrorMessage(error);
       }
@@ -48,10 +90,163 @@ function SearchBar({ onQuery }) {
             value={query}
             onChange={handleQueryChange}
             placeholder="Your Search to anywhere..."
+            autoComplete="off"
           />
+          <button
+            type="button"
+            className="searchbar__query-button"
+            onClick={handleQueryListClick}
+            aria-label="search type"
+          >
+            Search type
+            <img
+              src={singleDownArrow}
+              className={`searchbar__icon ${isQueryListOpen ? 'searchbar__icon_active' : ''} `}
+              alt="list arrow"
+            />
+          </button>
+          {isQueryListOpen && (
+            <ul className="searchbar__list">
+              <img
+                src={
+                  queryType === 'interesting_places'
+                    ? `${smallTriangleSelected}`
+                    : `${smallTriangle}`
+                }
+                className="searchbar__list-triangle"
+                alt="list arrow"
+              />
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'interesting_places' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__all">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__all"
+                    className="searchbar__radio-button"
+                    aria-label="all"
+                    value="interesting_places"
+                    onChange={handleQueryTypeChange}
+                  />
+                  All
+                </label>
+              </li>
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'architecture' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__architecture">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__architecture"
+                    className="searchbar__radio-button"
+                    aria-label="architecture"
+                    value="architecture"
+                    onChange={handleQueryTypeChange}
+                  />
+                  Architecture
+                </label>
+              </li>
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'museums' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__museums">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__museums"
+                    className="searchbar__radio-button"
+                    aria-label="museums"
+                    value="museums"
+                    onChange={handleQueryTypeChange}
+                  />
+                  Museums
+                </label>
+              </li>
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'theatres_and_entertainment' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__theatres_and_entertainment">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__theatres_and_entertainment"
+                    className="searchbar__radio-button"
+                    aria-label="theatres and entertainment"
+                    value="theatres_and_entertainment"
+                    onChange={handleQueryTypeChange}
+                  />
+                  Theaters and entertainment
+                </label>
+              </li>
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'natural' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__natural">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__natural"
+                    className="searchbar__radio-button"
+                    aria-label="natural locations"
+                    value="natural"
+                    onChange={handleQueryTypeChange}
+                  />
+                  Natural locations
+                </label>
+              </li>
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'surfing' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__surfing">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__surfing"
+                    className="searchbar__radio-button"
+                    aria-label="surfing spots"
+                    value="surfing"
+                    onChange={handleQueryTypeChange}
+                  />
+                  Surfing spots
+                </label>
+              </li>
+              <li
+                className={`searchbar__list-item ${
+                  queryType === 'restaurants' ? 'searchbar__list-item_selected' : ''
+                }`}
+              >
+                <label className="searchbar__label" htmlFor="querytype__restaurants">
+                  <input
+                    type="radio"
+                    name="querytype"
+                    id="querytype__restaurants"
+                    className="searchbar__radio-button"
+                    aria-label="restaurants"
+                    value="restaurants"
+                    onChange={handleQueryTypeChange}
+                  />
+                  Restaurants
+                </label>
+              </li>
+            </ul>
+          )}
           {errorMessage && <span className="searchbar__error">{errorMessage}</span>}
         </div>
-        <button type="submit" className="searchbar__button" aria-label="submit">
+        <button type="submit" className="searchbar__submit-button" aria-label="submit">
           Go!
         </button>
       </form>
--- src/utils/openTripApi.js
+++ src/utils/openTripApi.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://api.opentripmap.com/0.1/en/places';
 export const API_KEY = '5ae2e3f221c38a28845f05b6fd4cf53d0344c5ac0cdf7e621bc6ee07';
 export const SEARCH_RADIUS = '5000';
-export const DATA_SOURCE = 'wikidata';
+export const DATA_SOURCE = 'osm';
 export const RATING = '3';
 export const RESPONSE_FORMAT = 'json';
 export const RESPONSE_LIMIT = '3';
@@ -14,44 +14,51 @@ const getResponseData = (res) => {
 };
 
 // GET LOCATION COORDINATES FROM QUERIED NAME //
-const getLocationCoordinates = (locationName) => fetch(`${BASE_URL}/geoname?name=${locationName}&apikey=${API_KEY}`, {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
-})
-  .then((res) => getResponseData(res));
+const getLocationCoordinates = (locationName) =>
+  fetch(`${BASE_URL}/geoname?name=${locationName}&apikey=${API_KEY}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => getResponseData(res));
 
 // CREATE A RADIUS AROUND LOCATION COORDINATES AND RECEIVE PLACES OF INTEREST IDs IN RESPONSE //
-const createLocationRadius = (locationCoordinates) => fetch(`${BASE_URL}/radius?radius=${SEARCH_RADIUS}
+const createLocationRadius = (locationCoordinates, locationType) =>
+  fetch(
+    `${BASE_URL}/radius?radius=${SEARCH_RADIUS}
   &lon=${locationCoordinates.lon}&lat=${locationCoordinates.lat}
-  &src_geom=${DATA_SOURCE}&src_attr=${DATA_SOURCE}&rate=${RATING}
-  &format=${RESPONSE_FORMAT}&limit=${RESPONSE_LIMIT}&apikey=${API_KEY}`, {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
-})
-  .then((res) => getResponseData(res));
+  &src_geom=${DATA_SOURCE}&src_attr=${DATA_SOURCE}&kinds=${locationType}&rate=${RATING}
+  &format=${RESPONSE_FORMAT}&limit=${RESPONSE_LIMIT}&apikey=${API_KEY}`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  ).then((res) => getResponseData(res));
 
 // GET SPECIFIC INFO ABOUT RECEIVED PLACES OF INTEREST IDs //
-const getLocationsInfo = (locations) => Promise.all(locations.map((location) => fetch(`${BASE_URL}/xid/${location.xid}?apikey=${API_KEY}`, {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
-})))
-  .then((res) => Promise.all(res.map((item) => getResponseData(item))));
+const getLocationsInfo = (locations) =>
+  Promise.all(
+    locations.map((location) =>
+      fetch(`${BASE_URL}/xid/${location.xid}?apikey=${API_KEY}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+    )
+  ).then((res) => Promise.all(res.map((item) => getResponseData(item))));
 
 // COMPLETE FUNCTION TO BE USED UPON QUERY SUBMISSION //
-export async function getLocations(locationName) {
+export async function getLocations(locationName, locationType) {
   let returnedLocations = [];
 
   await getLocationCoordinates(locationName)
-    .then((locationCoordinates) => createLocationRadius(locationCoordinates))
+    .then((locationCoordinates) => createLocationRadius(locationCoordinates, locationType))
     .then((locationRadius) => getLocationsInfo(locationRadius))
     .then((locations) => {
       returnedLocations = locations;
