5b7485c3919788cd646132c70b3f2cd9128a00f7
Fred Jodozi <fredfred776@gmail.com>
2022-12-13 14:08:17 -0500

    add show more locations functionality

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import LocationCard from '../LocationCard/LocationCard';
 import './Locations.css';
 
 function Locations({ locations, onCardClick, onCardSave, onCardDelete, isLoggedIn }) {
+  const [visible, setVisible] = useState(3);
+
+  const handleShowMoreClick = () => {
+    setVisible(visible + 3);
+  };
+
   return (
     <section className="locations">
       <ul className="locations__list">
-        {locations.map((location) => (
+        {locations.slice(0, visible).map((location) => (
           <LocationCard
             key={location.xid}
             location={location}
@@ -16,6 +23,13 @@ function Locations({ locations, onCardClick, onCardSave, onCardDelete, isLoggedI
           />
         ))}
       </ul>
+      {locations.length > visible ? (
+        <button type="button" className="locations__button" onClick={handleShowMoreClick}>
+          Show more
+        </button>
+      ) : (
+        ''
+      )}
     </section>
   );
 }
